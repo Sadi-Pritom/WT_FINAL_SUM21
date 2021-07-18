71829f00977b30ccb198fa5638d7fb539b58a38a
Sadi-Pritom <sadipritom@gmail.com>
2021-07-18 13:56:47 +0600

    mvc lab task

---
+++ MVC lab task/Controllers/studentControl.php
@@ -0,0 +1,21 @@
+<?php
+    include '../Models/DBConfig.php';
+    $name="";
+    $err_name="";
+	$did="";
+    $err_did="";
+    $dob="";
+    $err_dob="";
+	$credit="";
+    $err_credit="";
+    $cgpa="";
+    $err_cgpa="";
+	$err_db="";
+	$hasError = false;
+
+   
+	}
+	
+	
+	
+?>
